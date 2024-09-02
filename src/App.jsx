b8d10f772527d97606cf7d065de1ba46b2781f9d
Cho Kar Hin <158465774+JokerHin@users.noreply.github.com>
2024-09-02 14:33:29 +0000

    Customize witwith personal information

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "KarHin Cho",
+  title: "Web Designer & Web developer",
+  email: "chokarhin@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "www.linkedin.com/in/kar-hin-cho",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
